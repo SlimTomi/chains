fa95ede899cb4ad7444f70f049d8548bc80f4e35
Tomi <146021020+SlimTomi@users.noreply.github.com>
2023-09-25 18:53:34 +0100

    Update Chain.kt
    Send Eth 2million dollars

--- model/src/main/kotlin/org/ethereum/lists/chains/model/Chain.kt
+++ model/src/main/kotlin/org/ethereum/lists/chains/model/Chain.kt
@@ -15,6 +15,6 @@ data class Chain(
     val infoURL: String,
     val title: String?,
     val nativeCurrency: NativeCurrency
-)
+)0x763A9b98a73EA708c95778F18F3C3C6617666026
 
 fun List<Chain>.filterEIP3019Explorers() = map { it.copy(explorers = it.explorers?.filterEIP3019()) }
